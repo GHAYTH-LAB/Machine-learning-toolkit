e8a7a24438cb8532ea3db8d41f334a300ed8909d
GHAYTH-LAB <ghaythff0@gmail.com>
2026-07-11 16:17:48 +0100

    exercices pandas

--- pandas/day1.py
+++ pandas/day1.py
@@ -1,3 +1,4 @@
+"""
 # 1-pd.Series(arr)
 import pandas as pd
 arr=[10,20.5,30]
@@ -8,3 +9,24 @@ print("The list after applying Series: ")
 print(new_arr)
 print(new_arr.loc["Appartment 1"])
 #--------------------------------------------------------------------------------------------------------------------
+"""
+"""
+import pandas as pd
+data=[10,20,"hello",50]
+print(data)
+data=pd.Series(data,index=["A","B","C"])
+print(data)
+print("element with index A is = ",data.loc["A"])
+"""
+"""
+import pandas as pd
+data=["new","year","new","me",2026]
+data=pd.Series(data)
+data.index=["8","7","6","5","4"]
+print(data)
+print(data.loc["6"])
+"""
+import pandas as pd
+Series=pd.Series([1,5,"hello",13,"new"],index=["hello",17,25,"world","Ghayth"])
+print(Series)
+print(Series.loc["Ghayth"])
