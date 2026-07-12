1a4a01d365d2ac61d7c8841f2e5d0905419d9b81
GHAYTH-LAB <ghaythff0@gmail.com>
2026-07-12 20:54:05 +0100

    final-code-of-day-3-learning-pandas

---
+++ pandas/day3.py
@@ -0,0 +1,33 @@
+import pandas as pd
+#creating a Dictionnary named Data
+Data={
+    "Name":["Ghayth","ALBERT","Yassine"],
+    "age":[19,30,22],
+    "job":["swe engeneer","teacher","police"]
+}
+#convert a dictionnary into a DataFrame
+df=pd.DataFrame(Data)
+print(df)
+#changing the row indexes 
+df.index=["First_employee","Second_employee","Third_employee"]
+print(df)
+#adding New Column named married
+df["married"]=[0,1,0]
+print(df)
+print("---------------------")
+#printing the row that contains Second employee label
+print(df.loc["Second_employee"])
+print("---------------------")
+#adding a new column named Score
+df["score"]=[15,20,25]
+print(df)
+#adding a new row with an index
+New_Row=pd.DataFrame([{"Name":"houssem"
+         ,"age":27
+         ,"job":"instructor"
+         ,"married":1
+         ,"score":37
+         }], index=["Fourth_employee"])
+df=pd.concat([df,New_Row])
+print("final DataFrame is : ")
+print(df)
