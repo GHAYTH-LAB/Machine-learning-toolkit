49271c41f1b482df2ef974454d6c7f5ac57cd825
GHAYTH-LAB <ghaythff0@gmail.com>
2026-07-12 23:11:25 +0100

    new-updates_to_day3

--- pandas/day3.py
+++ pandas/day3.py
@@ -6,3 +6,20 @@ Data={
     }
 Data=pd.DataFrame(Data,index=["First_Person","Second_Person","Third_Person","Fourth_Person","Fifth_Person","Sixth_Person"])
 print(Data)
+print(Data.loc["First_Person"])
+print(Data["age"])
+Data["married"]=[1,0,1,1,0,0]
+New_row=pd.DataFrame([{
+    "Name":"Hamza"
+    ,"age":22
+    ,"city":"Sfax"
+    ,"married":0
+}],index=["SEVENTH PERSON"])
+Data=pd.concat([Data,New_row])
+Data.loc["Ninth_person"]={
+    "Name":"hazem"
+    ,"age":45
+    ,"city":"MONASTIR"
+    ,"married":1
+}
+print(Data)
