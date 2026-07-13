31853e694b9f4f439f3b2b333dd08f8dfa6c1465
GHAYTH-LAB <ghaythff0@gmail.com>
2026-07-13 22:57:36 +0100

    day4-more code not complet

--- pandas/day4.py
+++ pandas/day4.py
@@ -4,5 +4,26 @@ df=pd.read_csv(r"C:\Users\abidli\Desktop\Machine learning toolkit\datasets\quebe
 print(df.to_string())
 print("--------------------")
 #How to read a json file using pandas 
-df=pd.read_json(r"C:\Users\abidli\Desktop\Machine learning toolkit\datasets\students.json")
-print(df)
+df1=pd.read_json(r"C:\Users\abidli\Desktop\Machine learning toolkit\datasets\students.json")
+print(df1)
+#selection BY column
+print(df1["name"])
+print("---------------------")
+#Selection by column and printing all the cities
+print(df["city"].to_string())
+#select by multiple columns
+print(df[["bedrooms","bathrooms","garage"]].to_string())
+#select per row
+print(df.loc[2])
+print("---------------------")
+print(df1.iloc[2])
+print("----------------------")
+import pandas as pd
+Data=pd.read_csv(r"C:\Users\abidli\Desktop\Machine learning toolkit\assets\WorldCupMatches.csv",index_col="City")
+print(Data)
+print("----------------------")
+print(Data.loc["Sao Paulo "])
+print("----------------------")
+print(Data["Stadium"])
+print("-----------------------")
+print(Data[["Home Team Name","Away Team Name","Year"]])
