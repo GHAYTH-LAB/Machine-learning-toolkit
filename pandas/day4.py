6d74d43e7b0417b85ab31a3492bd72eb8021ecf3
GHAYTH-LAB <ghaythff0@gmail.com>
2026-07-14 04:00:41 +0100

    new_files

--- pandas/day4.py
+++ pandas/day4.py
@@ -2,3 +2,19 @@
 import pandas as pd
 df=pd.read_csv(r"C:\Users\abidli\Desktop\Machine learning toolkit\datasets\results.csv")
 print(df.to_string())
+#select a row
+print(df.loc[1])
+#select a column
+print(df["home_team"])
+#Select rows
+print(df.loc[[0,10,15]])
+print("--------------")
+print(df.loc[0:16])
+#select by columns
+print(df["home_team"])
+print(df[["home_team","away_team","result"]].to_string())
+#select specific rows+columns
+print(df.loc[5:16,["home_team","away_team","result"]])
+import pandas as pd;
+df=pd.read_json(r"C:\Users\abidli\Desktop\Machine learning toolkit\datasets\Dataset.json")
+print(df)
