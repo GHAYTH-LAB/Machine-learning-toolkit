f0c195b378610a18c8a7a29f7650d548d431d354
GHAYTH-LAB <ghaythff0@gmail.com>
2026-07-15 03:23:54 +0100

    New_changes

--- pandas/day4.py
+++ pandas/day4.py
@@ -1,27 +1,53 @@
-
+"""import pandas as pd
+Dict={
+    "Game":["Forza Horizon","Fifa 19","red Dead redemption"]
+    ,"Edition":[2019,2020,2022]
+    ,"version":[3,4,8]
+}
+Dict=pd.DataFrame(Dict)
+Dict.index=["info_1","info_2","info_3"]
+print(Dict)
+print("----------------")
+print(Dict.loc["info_1"])
+print(Dict.iloc[2])
+print("-----------------")
+print(Dict["Edition"])
+print(Dict["Game"])
+Dict.loc["info_1"]={
+    "Game":"Pes"
+    ,"Edition":2025
+    ,"version":5   
+}
+print("-----------------")
+print(Dict)
+Dict["version"]=[21,17,2]
+print("------------------")
+print(Dict)
+New_rows={
+    "Game":["Blur","Gta San andreas"]
+    ,"Edition":[2012,2006]
+    ,"version":[14,20]
+}
+New_rows=pd.DataFrame(New_rows,index=["info_4","info_5"])
+Dict=pd.concat([Dict,New_rows])
+print(Dict)
+print("---------------------")
+Dict["Name"]=["Ghayth","Anass","Mohamed","Aziz","Rayen"]
+print(Dict)
+"""
 import pandas as pd
-df=pd.read_csv(r"C:\Users\abidli\Desktop\Machine learning toolkit\datasets\results.csv")
-print(df.to_string())
-#select a row
+df=pd.read_csv(r"C:\Users\abidli\Desktop\Machine learning toolkit\datasets\google_playstore_2026.csv")
+print(df)
+print("---------------------")
+print(df.iloc[0])
+print("---------------------")
 print(df.loc[1])
-#select a column
-print(df["home_team"])
-#Select rows
-print(df.loc[[0,10,15]])
-print("--------------")
-print(df.loc[0:16])
-#select by columns
-print(df["home_team"])
-print(df[["home_team","away_team","result"]].to_string())
-#select specific rows+columns
-print(df.loc[5:16,["home_team","away_team","result"]])
-import pandas as pd;
-df=pd.read_csv(r"C:\Users\abidli\Desktop\Machine learning toolkit\datasets\quebec_housing_sales_v2.csv",index_col="city")
-print(df.iloc[3])
-print("-------------")
-print(df.loc["Montréal"])
-print(df["bedrooms"])
-print(df.iloc[[2,5,7]])
-print(df.iloc[2:6])
-print(df[["neighborhood","year_built"]])
-print(df.loc[df.index[2:8] ,["bathrooms","garage"]])
+print(df.loc[[1,3,5]])
+print("---------------------")
+print(df.loc[2:5])
+print("----------------------")
+print(df[["App","Rating"]])
+print("----------------------")
+print(df.loc[0:4,["App","Rating","Category"]])
+print("----------------------")
+print(df.loc[120:125,["App","Category","Rating"]])
