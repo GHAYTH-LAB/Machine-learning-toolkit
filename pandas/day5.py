ee4de9f079dea2ab3c63043abf9600d3a3994a2d
GHAYTH-LAB <ghaythff0@gmail.com>
2026-07-16 04:13:51 +0100

    initial day5 commit

--- pandas/day5.py
+++ pandas/day5.py
@@ -27,3 +27,25 @@ print(df[(df["successful_response"] == 1) & (df["application_domain"] == "Coding
 print("--------------------------")
 #using the or operator (|)
 print(df[(df["application_domain"]=="coding") | (df["application_domain"]=="Education")])
+#How to summerize and analyse data
+df=pd.read_csv(r"C:\Users\abidli\Desktop\Machine learning toolkit\datasets\weight-height.csv")
+#How to get the min value?
+print(df.min(numeric_only=True))
+#How to get the max value?
+print(df.max(numeric_only=True))
+#How to get the mean value?
+print(df.mean(numeric_only=True))
+#How to get the sum value?
+print(df.sum(numeric_only=True))
+#How to get the median?
+print(df.median(numeric_only=True))
+#How to get the mode(most frequent value)?
+print(df.mode())
+#How to get how many non Null values
+print(df.count(numeric_only=True))
+#The function that groups them all
+df=pd.read_csv(r"C:\Users\abidli\Desktop\Machine learning toolkit\datasets\dubai_residential_data_2026.csv")
+print(df.describe(include="all"))
+print("---------------------")
+#For a specific colomn
+print(df["TRANS_VALUE"].describe())
