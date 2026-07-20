11c9a10c5b3b8e34bf162cb43d145bbdc52a651f
GHAYTH-LAB <ghaythff0@gmail.com>
2026-07-20 18:07:24 +0100

    full code of day6 +updated day5 code

---
+++ numpy/day6.py
@@ -0,0 +1,19 @@
+import numpy as np
+# np.sort()  Numerical
+np1 = np.array([6,7,4,9,0,2,1])
+print(np1)
+print(np.sort(np1))
+# Alphabetical
+np2 = np.array(["John", "Tina", "Aaron", "Zed"])
+print(np2)
+print(np.sort(np2))
+# Booleans T/F
+np3 = np.array([True, False, False, True])
+print(np3)
+print(np.sort(np3))
+# Return a copy not change the original
+print(np1)
+print(np.sort(np1))
+print(np1)
+# Sort in descending order
+print(np.sort(np1)[::-1])
