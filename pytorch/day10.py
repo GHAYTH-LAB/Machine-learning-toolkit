846514ad39f7c09e4f0b9de7cb9137594552fc99
GHAYTH-LAB <ghaythff0@gmail.com>
2026-08-04 22:30:12 +0100

    Pytorch trial

--- pytorch/day10.py
+++ pytorch/day10.py
@@ -3,12 +3,81 @@ import numpy as np
 import torch.nn as nn
 import torch 
 import torch.nn.functional as F
+import torch.optim as optim
 from torch.utils.data import TensorDataset,DataLoader
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler,OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score,f1_score,precision_score
 from skorch import NeuralNetClassifier
 torch.manual_seed(41)
 device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
-df=pd.read_csv(r"C:\Users\abidli\Desktop\Machine learning toolkit\datasets\employee_dataset_50000.csv")
+df=pd.read_csv(r"C:\Users\abidli\Desktop\Machine learning toolkit\datasets\employee_dataset_50000_imputed.csv")
 #data handling and preporcessing
+df.columns=df.columns.str.lower()
+y=df["attrition"]
+id_extracted=df["employeeid"]
+X=df.drop(columns=["employeeid","attrition"])
+class_names=y.unique()
+print(class_names)
+class_to_index={
+    name:i for i,name in enumerate(class_names)
+}
+y = y.map(class_to_index)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+num_cols=X_train.select_dtypes(exclude=["str","object"]).columns
+cat_cols=X_train.select_dtypes(include=["str","object"]).columns
+scaler=MinMaxScaler()
+X_train_num=scaler.fit_transform(X_train[num_cols])
+X_test_num=scaler.transform(X_test[num_cols])
+encoder=OneHotEncoder(
+    sparse_output=False
+    ,handle_unknown="ignore"
+)
+X_train_cat=encoder.fit_transform(X_train[cat_cols])
+X_test_cat=encoder.transform(X_test[cat_cols])
+X_train=np.hstack([X_train_num,X_train_cat])
+X_test=np.hstack([X_test_num,X_test_cat])
+X_train=torch.from_numpy(X_train).float()
+X_test=torch.from_numpy(X_test).float()
+y_train=torch.from_numpy(y_train.values).float().unsqueeze(1)
+y_test=torch.from_numpy(y_test.values).float().unsqueeze(1)
+train_dataset=TensorDataset(X_train,y_train)
+train_loader=DataLoader(train_dataset,shuffle=True,batch_size=32)
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1=nn.Linear(X_train.shape[1],64)
+        self.layer2=nn.Linear(64,128)
+        self.layer3=nn.Linear(128,32)
+        self.layer4=nn.Linear(32,16)
+        self.layer5=nn.Linear(16,1)
+    def forward(self,x):
+        x=F.relu(self.layer1(x))
+        x=F.relu(self.layer2(x))
+        x=F.leaky_relu(self.layer3(x))
+        x=F.relu(self.layer4(x))
+        x=self.layer5(x)
+        return x
+model=NeuralNetwork()
+criterion=nn.BCEWithLogitsLoss()
+optimizer=optim.Adam(model.parameters(),lr=0.001)
+epochs=200
+for epoch in range(epochs):
+    model.train()
+    curr_loss=0.00
+    for x_batch,y_batch in train_loader:
+        optimizer.zero_grad()
+        predictions=model(x_batch)
+        loss=criterion(predictions,y_batch)
+        loss.backward()
+        optimizer.step()
+        curr_loss+=loss.item()
+    if(epoch%10==0):
+        print(f"epoch {epoch}  average loss={curr_loss/len(train_loader)} ")
+with torch.no_grad():
+    model.eval()
+    predictions=model(X_test)
+    predictions=torch.sigmoid(predictions)
+    predicted_labels = (predictions >= 0.5).float()
+    loss=criterion(predictions,y_test)
+    print(f"loss= {loss.item()} model accuracy {accuracy_score(y_test.numpy(),predicted_labels.numpy())} f1_score= {f1_score(y_test.numpy(),predicted_labels.numpy(),average="weighted")}")
--- pytorch/day9.py
+++ pytorch/day9.py
@@ -1,79 +1,76 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import QuantileTransformer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score,accuracy_score
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import TensorDataset,DataLoader
-df=pd.read_csv(r"C:\Users\abidli\Desktop\Machine learning toolkit\datasets\creatures_dataset.csv")
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,precision_score,f1_score
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+df=pd.read_csv(r"C:\Users\abidli\Desktop\Machine learning toolkit\datasets\employee_dataset_50000_imputed.csv")
 torch.manual_seed(42)
-print(df.columns)
-y=df["creature_type"]
-X=df.drop(columns=["id","creature_type"])
-class_names = y.unique()
-class_to_index = {
-    name: i for i, name in enumerate(class_names)
+df.columns=df.columns.str.lower()
+class_names=df["attrition"].unique()
+y=df["attrition"]
+id_extracted=df["employeeid"]
+X=df.drop(columns=["attrition","employeeid"])
+class_to_index={
+    name:i for i,name in enumerate(class_names)
 }
 y = y.map(class_to_index)
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
-scaler=QuantileTransformer(n_quantiles=500)
-X_train=scaler.fit_transform(X_train)
-X_test=scaler.transform(X_test)
+X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=42,test_size=0.2)
+num_cols=X_train.select_dtypes(exclude=["str","object"]).columns
+cat_cols=X_train.select_dtypes(include=["str","object"]).columns
+scaler=StandardScaler()
+X_train_num=scaler.fit_transform(X_train[num_cols])
+X_test_num=scaler.transform(X_test[num_cols])
+encoder=OneHotEncoder(sparse_output=False,handle_unknown="ignore")
+X_train_cat=encoder.fit_transform(X_train[cat_cols])
+X_test_cat=encoder.transform(X_test[cat_cols])
+X_train=np.hstack([X_train_num,X_train_cat])
+X_test=np.hstack([X_test_num,X_test_cat])
 X_train=torch.from_numpy(X_train).float()
 X_test=torch.from_numpy(X_test).float()
-y_train=torch.from_numpy(y_train.values).long()
-y_test=torch.from_numpy(y_test.values).long()
+y_train=torch.from_numpy(y_train.values).float().unsqueeze(1)
+y_test=torch.from_numpy(y_test.values).float().unsqueeze(1)
 train_dataset=TensorDataset(X_train,y_train)
-data_loader=DataLoader(train_dataset,batch_size=16,shuffle=True)
+train_loader=DataLoader(train_dataset,shuffle=True,batch_size=32)
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.layer1=nn.Linear(X_train.shape[1],64)
         self.layer2=nn.Linear(64,128)
-        self.layer3=nn.Linear(128,32)
-        self.layer4=nn.Linear(32,64)
+        self.layer3=nn.Linear(128,16)
+        self.layer4=nn.Linear(16,64)
         self.layer5=nn.Linear(64,32)
-        self.layer6=nn.Linear(32,4)
+        self.layer6=nn.Linear(32,1)
     def forward(self,x):
         x=F.relu(self.layer1(x))
         x=F.relu(self.layer2(x))
-        x=F.tanh(self.layer3(x))
-        x=F.tanh(self.layer4(x))
-        x=F.leaky_relu(self.layer5(x))
-        x=self.layer6(x)
+        x=F.relu(self.layer3(x))
+        x=F.relu(self.layer4(x))
+        x=F.relu(self.layer5(x))
+        x=F.sigmoid(self.layer6(x))
         return x
 model=NeuralNetwork()
-criterion=nn.CrossEntropyLoss()
-optimizer=optim.Adam(model.parameters(),lr=0.001)
-epochs=150
+criterion=nn.BCELoss()
+optimizer=optim.Adam(model.parameters(),lr=0.005)
+epochs=120
 for epoch in range(epochs):
     model.train()
     curr_loss=0.00
-    for x_batch,y_batch in data_loader:
+    for x_batch,y_batch in train_loader:
         optimizer.zero_grad()
         predictions=model(x_batch)
         loss=criterion(predictions,y_batch)
         loss.backward()
         optimizer.step()
         curr_loss+=loss.item()
-    if (epoch%10==0):
-        print(f"epoch {epoch}  average_loss= {curr_loss/len(data_loader)}")
+    print(f"epoch {epoch} loss= {curr_loss/len(train_loader)}")
 with torch.no_grad():
     model.eval()
     predictions=model(X_test)
-    predicted_class_position=torch.argmax(predictions,dim=1)
-predicted_labels=np.array(class_names[predicted_class_position.numpy()])
-loss=criterion(predictions,y_test)
-print("loss",loss.item())
-y_pred = predicted_class_position.numpy()
-y_true = y_test.numpy()
-
-print(f"accuracy= {accuracy_score(y_true,y_pred)} f1 score = {f1_score(y_true,y_pred,average='weighted')}")
-prediction=pd.DataFrame({
-    "id":range(1,len(predicted_labels)+1)
-    ,"predictions":predicted_labels
-})
-prediction.to_csv("predictions.csv",index=False)
+    loss=criterion(predictions,y_test)
+    predicted_labels=(predictions>=0.5).float()
+    print(f"loss= {loss.item()} accuracy = {accuracy_score(y_test.numpy(),predicted_labels.numpy())}")
